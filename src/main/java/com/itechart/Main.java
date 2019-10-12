6b5ee4a3be087a50c396fe76db68bbda22f7814b
Yulia <44472434+javdeeva@users.noreply.github.com>
2019-10-12 20:11:13 +0300

    Bubble sort and output implementation

--- src/main/java/com/itechart/Main.java
+++ src/main/java/com/itechart/Main.java
@@ -1,9 +1,22 @@
 package main.java.com.itechart;
 
+import main.java.com.itechart.lesson2.ArrayBubbleSort;
+import main.java.com.itechart.lesson2.MinMaxAvgValueOfArray;
+import main.java.com.itechart.lesson2.RandomArray;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
+        RandomArray randomArray = new RandomArray();
+        randomArray.printRandomArray(randomArray.randomArray);
+
+        MinMaxAvgValueOfArray values = new MinMaxAvgValueOfArray();
+        values.printMinMaxAvgValues(randomArray.randomArray);
+
+        ArrayBubbleSort sort = new ArrayBubbleSort();
+        sort.printSortedArray(sort.bubbleSort(randomArray.randomArray));
     }
 }
