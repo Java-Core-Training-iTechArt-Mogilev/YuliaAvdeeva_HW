3f043e313baf3aa1672e8cc3ab6735d508c0979b
Yulia <44472434+javdeeva@users.noreply.github.com>
2019-10-13 22:13:05 +0300

    Changes after code review

--- src/main/java/com/itechart/Main.java
+++ src/main/java/com/itechart/Main.java
@@ -1,22 +1,33 @@
 package main.java.com.itechart;
 
-import main.java.com.itechart.lesson2.ArrayBubbleSort;
-import main.java.com.itechart.lesson2.MinMaxAvgValueOfArray;
-import main.java.com.itechart.lesson2.RandomArray;
+import main.java.com.itechart.lesson2.ArrayUtils;
+
+import java.util.Scanner;
 
 public class Main {
+
+    public static int getArrayLengthFromConsole() {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter array length:");
+        int arrayLength = in.nextInt();
+        in.close();
+        return arrayLength;
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
-        RandomArray randomArray = new RandomArray();
-        randomArray.printRandomArray(randomArray.randomArray);
+        double[] array = ArrayUtils.createRandomArray(getArrayLengthFromConsole());
+        System.out.println("Generated array:");
+        ArrayUtils.printArray(array);
+        System.out.println("Min value is " + ArrayUtils.findMinValue(array));
+        System.out.println("Max value is " + ArrayUtils.findMaxValue(array));
+        System.out.println("Avg value is " + ArrayUtils.findAvgValue(array));
+        System.out.println("Sorted array:");
+        ArrayUtils.printArray(ArrayUtils.bubbleSort(array));
 
-        MinMaxAvgValueOfArray values = new MinMaxAvgValueOfArray();
-        values.printMinMaxAvgValues(randomArray.randomArray);
-
-        ArrayBubbleSort sort = new ArrayBubbleSort();
-        sort.printSortedArray(sort.bubbleSort(randomArray.randomArray));
     }
 }
